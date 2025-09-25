7df25d9add83be062c609eee06a2d54958427ee5
Etienne <40747@ma-web.nl>
2025-09-25 10:36:25 +0200

    m1 prog third commit

---
+++ 03_rekenen/output/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace output;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 1;
+        double b = 1.9;
+        float vx = 9.87f;
+        bool ja = true;
+
+        Console.WriteLine($"mijn int met de naam a heeft als waarde {a}");
+        Console.WriteLine($"mijn double met de naam b heeft als waarde {b}");
+        Console.WriteLine($"mijn float met de naam vx heeft als waarde {vx}");
+        Console.WriteLine($"mijn bool met de naam ja heeft als waarde {ja}");
+        Console.WriteLine($"mijn bankrekening heeft {b * vx} duizend euro");
+    }
+}
