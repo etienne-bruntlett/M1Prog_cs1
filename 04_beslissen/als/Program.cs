4a6c6c03dec920fd75c14a3620457f4af4290530
Etienne <40747@ma-web.nl>
2025-10-02 09:58:10 +0200

    commit 6

---
+++ 04_beslissen/als/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace als;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("- Je zoekt je sleutel.");
+        bool sleutelkwijt = false;
+        if (sleutelkwijt == true)
+        {
+            Console.WriteLine("- Je bent je sleutel kwijt!");
+            Console.WriteLine("- De deur blijft gesloten.");
+        }
+        else
+        {
+            Console.WriteLine("- Je hebt je sleutel gevonden!");
+            Console.WriteLine("- Je kan de deur openen.");
+        }
+    }
+}
