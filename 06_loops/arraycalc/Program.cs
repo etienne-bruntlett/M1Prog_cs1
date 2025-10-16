1b15c8e21bd2609a473860888b3860e8b8ea5e88
Etienne <40747@ma-web.nl>
2025-10-16 09:55:26 +0200

    commit 12

---
+++ 06_loops/arraycalc/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace arraycalc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] saldo = new double[] { 240.99, 1142.23, 789.89, 658.72 };
+
+        for (int i = 0 ; i < saldo.Length; i++)
+        {
+            double verhogingSaldo = saldo[i] + 100;
+            Console.WriteLine($"{i} saldo na verhoging {verhogingSaldo}");
+        }
+    }
+}
