4b495200c7889d5b5220b7d74003f05360a64ddb
Etienne <40747@ma-web.nl>
2025-10-16 10:06:16 +0200

    commit 13

---
+++ 06_loops/forif/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace forif;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nummers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        for (int i = 3 ; i < 8; i++)
+        {
+            int numbers = nummers[i];
+            Console.WriteLine($"{numbers}");
+        }
+    }
+}
