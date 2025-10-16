fa34a989c18764259ac13614da6ef439edb62a69
Etienne <40747@ma-web.nl>
2025-10-16 10:51:48 +0200

    commit 16

---
+++ 06_loops/stappen/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace stappen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float[] vectors = new float[] {
+           6, 7, 8,
+           1, 4, 0
+       };
+
+        for (int i = 0; i < vectors.Length; i+=3)
+        {
+            float vector = vectors[i];
+            Console.WriteLine($"vector vanaf index {i}: ({vectors[i]}, {vectors[i+1]}, {vectors[i+2]})");
+        }
+    }
+}
