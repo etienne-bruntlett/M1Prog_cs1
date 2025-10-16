f43060323375bbc6598028bd89fe92c1d2e3460f
Etienne <40747@ma-web.nl>
2025-10-16 10:17:09 +0200

    commit 14

---
+++ 06_loops/stringclass/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace stringclass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] random = new string[] { "16", "VOORBEELD STRING", "global" };
+
+        string[] geenCaps = new string[] { "global", "variabele", "class" };
+
+        for (int i = 0; i < random.Length; i++)
+        {
+            string things = random[i];
+            Console.WriteLine($"{things}");
+        }
+
+        for (int i = 0; i < geenCaps.Length; i++)
+        {
+            string lijst = geenCaps[i];
+            Console.WriteLine(lijst);
+            Console.WriteLine($"is een string die {lijst.Length} letters lang is");
+            Console.WriteLine(lijst.ToUpper());
+        }
+    }
+}
